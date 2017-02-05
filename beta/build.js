a7de3a4fb7f13d749c5ce86838ac908e0f5ebd86
Hidden50 <hiddensnorespond@gmail.com>
2017-02-05 09:27:20 +0100

    builddata lookup: Search for precisely the right entry

--- beta/build.js
+++ beta/build.js
@@ -52,17 +52,17 @@ function packBuildData (setlists) {
 this.compendiumToHtmlImages = function compendiumToHtmlImages (compendium, build, options, species, set, mode, targetSpecies) {
 	let speciesMap =       aSpecies => compendiumToHtmlImages(compendium, build, options, aSpecies);
 	let setMap =           aSet =>     compendiumToHtmlImages(compendium, build, options, species, aSet);
-	let tilesMap =         aSet =>     brmtIcon(compendium, species, aSet);
+	let tilesMap =         aSet =>     brmtIcon(compendium, `${species} (${aSet})`, species, aSet);
 	let modeMap =          aMode =>    `<b>${aMode}</b>: ` + compendiumToHtmlImages(compendium, build, options, species, set, aMode);
 	let targetSpeciesMap = aSpecies => compendiumToHtmlImages(compendium, build, options, species, set, mode, aSpecies);
-	let targetSetMap =     aSet =>     brmtIcon(compendium, targetSpecies, aSet);
+	let targetSetMap =     aSet =>     brmtIcon(compendium, mode.endsWith("to") ? `${species} (${set}) beats ${targetSpecies} (${aSet})` : `${targetSpecies} (${aSet}) beats ${species} (${set})`, targetSpecies, aSet);
 	
 	if (!species)        return Object.keys(build).map( speciesMap ).join('');
 	if (!build[species]) return `Error: Species "${species}" not found.`;
 	if (options.includes("tiles"))
 		return Object.keys(build[species]).map( tilesMap ).join('');
 	if (!set)            return `<b>${species} Checks</b><br><hr>` + Object.keys(build[species]).map( setMap ).join('<br><hr>');
-	if (!mode)           return brmtIcon(compendium, species, set) + ` <b>${set}</b><br>` + Object.keys(build[species][set]).map( modeMap ).join('<br>');
+	if (!mode)           return brmtIcon(compendium, `${species} (${set})`, species, set) + ` <b>${set}</b><br>` + Object.keys(build[species][set]).map( modeMap ).join('<br>');
 //	if (!set)            return `<b>${species} Checks</b> (click to expand)<br>` + Object.keys(build[species]).map( setMap ).join('');
 //	if (!mode)           return htmlDetails( brmtIcon(compendium, species, set) + ` <b>${set}</b>`, Object.keys(build[species][set]).map( modeMap ).join('<br>') );
 	if (!targetSpecies)  return Object.keys(build[species][set][mode]).map( targetSpeciesMap ).join('');
@@ -74,23 +74,23 @@ const miniIconStyle =        'style="position:absolute; right:0px; bottom:0px"';
 
 let Weblink = (species, set) => `./../Serebii__Images/${species}.png`;
 
-function brmtIcon (compendium, species, set, cornerDisplay) {  // todo: wraplines, nowraplines, sets
+function brmtIcon (compendium, title, species, set, cornerDisplay) {  // todo: wraplines, nowraplines, sets
 	let image = `<img src="${Weblink(species, set)}">`;
 	if (!set) return image;
-	if (cornerDisplay) return `<span title="${species} (${set})" class="iconWrapper">${image}<span ${miniIconStyle}>${cornerDisplay}</span></span>`;
+	if (cornerDisplay) return `<span title="${title}" class="iconWrapper">${image}<span ${miniIconStyle}>${cornerDisplay}</span></span>`;
 	let miniIcon;
 	if (compendium.miniIcons[species] && compendium.miniIcons[species][set])
 		miniIcon = compendium.miniIcons[species][set];
 	else if (compendium.miniIcons[set])
 		miniIcon = compendium.miniIcons[set];
-	else return brmtIcon( compendium, species, set, `<b ${miniIconFontStyle}>${set}</b>` );
+	else return brmtIcon( compendium, title, species, set, `<b ${miniIconFontStyle}>${set}</b>` );
 	
 	if (miniIcon.letters !== undefined)
-		return brmtIcon( compendium, species, set, `<b ${miniIconFontStyle}>${miniIcon.letters}</b>` );
+		return brmtIcon( compendium, title, species, set, `<b ${miniIconFontStyle}>${miniIcon.letters}</b>` );
 	if (miniIcon.icon)
-		return brmtIcon( compendium, species, set, `<img src="${Weblink(miniIcon.icon)}" width=18px height=18px">` );
+		return brmtIcon( compendium, title, species, set, `<img src="${Weblink(miniIcon.icon)}" width=18px height=18px">` );
 	if (miniIcon.image)
-		return brmtIcon( compendium, miniIcon.image );
+		return brmtIcon( compendium, title, miniIcon.image );
 }
 
 /* Aliases */
--- beta/tools.js
+++ beta/tools.js
@@ -21,4 +21,57 @@ window.showPopout = function showPopout (containerID, Sender, contentHtml) {
 	var maxTop = window.pageYOffset + window.innerHeight - container.offsetHeight - 15;
 	container.style.top = maxTop + 'px';
 	container.style.top = (maxTop - Math.max(container.offsetTop - YOffset, 0)) + 'px';
+};
+
+window.addIconWrapListeners = function addIconWrapListeners (parent, eventType, listener) {
+	if (typeof parent === "string")
+		parent = document.getElementById(parent);
+	[...parent.getElementsByClassName("iconWrapper")].forEach(
+		iconWrapper => iconWrapper.addEventListener( eventType, () => listener(iconWrapper) )
+	);
+};
+
+window.addPopup = function addPopup (source, species, set) {
+	if (!species)
+		[, species, set] = source.title.match(/^(.*) \(([^()]*)\)$/);
+	showPopout(
+		"onclickinfo_popout",
+		source,
+		buildTools.compendiumToHtmlImages( compendiums.OUcc, build, [], species )
+	);
+	addIconWrapListeners(
+		"onclickinfo_popout",
+		'click',
+		scrollBuilddata
+	);
+}
+
+window.scrollBuilddata = function scrollBuilddata (species, set, defenderSpecies, defenderSet) {
+	if (species.title)  // title: "defenderSpecies (defenderSet) beats species (set)"
+		[, defenderSpecies, defenderSet, species, set] = species.title.match(/^([^()]*) \(([^()]*)\)(?: beats ([^()]*) \(([^()]*)\))?$/);
+	if (!species)       // title was "species (set)" instead
+		[species, set, defenderSpecies, defenderSet] = [defenderSpecies, defenderSet, "", ""];
+	textareaFindText( document.getElementById("builddata"), new RegExp("\n" + `(?=${species}[^\n]*${defenderSpecies})`, 'i') );
+	textareaFindText( document.getElementById("builddata"), new RegExp(defenderSpecies || species, 'i') );
+}
+
+window.textareaFindText = function textareaFindText (ta, regex) {
+	let offset = ta.selectionEnd;
+	let match = ta.value.substr(offset).match(regex);
+	if (!match) {
+		// reached the end. loop around and try again..
+		offset = 0;
+		match = ta.value.match(regex);
+	}
+	if (!match) return;  // no results found
+	let selStart = offset + match.index;
+	let selEnd = offset + match.index + match[0].length;
+	let temp = ta.value;
+	// scroll to position
+	ta.focus();
+	ta.value = temp.substring(0, selStart);
+	ta.scrollTop = selStart;
+	ta.value = temp;
+	// mark result for user
+	ta.setSelectionRange(selStart, selEnd);
 }
