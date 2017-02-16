845a5fb3239127f4bd56f5deed32393db9bfcfc9
Hidden50 <hiddensnorespond@gmail.com>
2017-02-16 23:21:19 +0100

    sort output by rough estimate of threat level

--- beta/build.js
+++ beta/build.js
@@ -65,19 +65,44 @@ window.packBuildData = function packBuildData (setlists, useOfficialNames) {
 
 /* Generate Html Output */
 
-this.compendiumToHtmlImages = function compendiumToHtmlImages (compendium, build, options, species, set, mode, targetSpecies) {
-	let speciesMap =       aSpecies => compendiumToHtmlImages(compendium, build, options, aSpecies);
-	let setMap =           aSet =>     compendiumToHtmlImages(compendium, build, options, species, aSet);
-	let tilesMap =         aSet =>     brmtIcon(compendium, `${window.compendiums.officialnames[species] || species} (${aSet})`, species, aSet);
-	let modeMap =          aMode =>    `<b>${aMode}</b>: ` + compendiumToHtmlImages(compendium, build, options, species, set, aMode);
-	let targetSpeciesMap = aSpecies => compendiumToHtmlImages(compendium, build, options, species, set, mode, aSpecies);
-	let beats =            (A, B) =>   mode.endsWith("to") ? `${A} beats ${B}` : `${B} beats ${A}`;
-	let targetSetMap =     aSet =>     brmtIcon(compendium, beats(`${window.compendiums.officialnames[species] || species} (${set})`, `${window.compendiums.officialnames[targetSpecies] || targetSpecies} (${aSet})`), targetSpecies, aSet);
+function sortThreats(build) {
+	let threats = [];
+	for (let species in build)
+	for (let set in build[species])
+		threats.push( [species, set] );
+	let count = (species, set, mode) => Object.keys(build[species][set][mode]).length;
+	let modes = ["GSI","SSI","NSI","GSI to","SSI to","NSI to"];
+	let weights = [300, 200, 2, -11, -7, -3];
+	threats =  threats.sort( (a,b) => {
+		let scoreA = 0, scoreB = 0;
+		for (m = 0; m < modes.length; m++) {
+			scoreA += weights[m] * count(a[0], a[1], modes[m]);
+			scoreB += weights[m] * count(b[0], b[1], modes[m]);
+			if (m === 2 && scoreA === 0)
+				scoreA = 500000;
+			if (m === 2 && scoreB === 0)
+				scoreB = 500000;
+		}
+		return scoreA - scoreB;
+	} );
+//	window.showPopout(  "onclickinfo_popout", usespeciesid, "<textarea>" + threats.map(JSON.stringify).join("\r\n") + "</textarea>"  );
+	return threats;
+}
+
+this.compendiumToHtmlImages = function compendiumToHtmlImages    (compendium, build, options, species, set, mode, targetSpecies) {
+	let speciesSetMap =    aSpeciesSet => compendiumToHtmlImages(compendium, build, options, aSpeciesSet[0], aSpeciesSet[1]);
+	let speciesMap =       aSpecies =>    compendiumToHtmlImages(compendium, build, options, aSpecies);
+	let setMap =           aSet =>        compendiumToHtmlImages(compendium, build, options, species, aSet);
+	let tilesMap =         aSet =>        brmtIcon(compendium, `${window.compendiums.officialnames[species] || species} (${aSet})`, species, aSet);
+	let modeMap =          aMode =>       `<b>${aMode}</b>: ` + compendiumToHtmlImages(compendium, build, options, species, set, aMode);
+	let targetSpeciesMap = aSpecies =>    compendiumToHtmlImages(compendium, build, options, species, set, mode, aSpecies);
+	let beats =            (A, B) =>      mode.endsWith("to") ? `${A} beats ${B}` : `${B} beats ${A}`;
+	let targetSetMap =     aSet =>        brmtIcon(compendium, beats(`${window.compendiums.officialnames[species] || species} (${set})`, `${window.compendiums.officialnames[targetSpecies] || targetSpecies} (${aSet})`), targetSpecies, aSet);
 	
-	if (!species)        return Object.keys(build).map( speciesMap ).join('');
+	if (!species)        return sortThreats(build).map( speciesSetMap ).join('');
 	if (!build[species]) return `Error: Species "${species}" not found.`;
 	if (options.includes("tiles"))
-		return Object.keys(build[species]).map( tilesMap ).join('');
+		return brmtIcon(compendium, `${window.compendiums.officialnames[species] || species} (${set})`, species, set);
 	if (!set)            return `<b>${window.compendiums.officialnames[species] || species} Checks</b><br><hr>` + Object.keys(build[species]).map( setMap ).join('<br><hr>');
 	if (!mode)           return brmtIcon(compendium, `${window.compendiums.officialnames[species] || species} (${set})`, species, set) + ` <b>${set}</b><br>` + Object.keys(build[species][set]).map( modeMap ).join('<br>');
 //	if (!set)            return `<b>${species} Checks</b> (click to expand)<br>` + Object.keys(build[species]).map( setMap ).join('');
