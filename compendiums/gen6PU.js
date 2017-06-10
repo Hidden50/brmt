6dd89b8ed909db0e5328734e9df6874a991e51fb
Hidden50 <hiddensnorespond@gmail.com>
2017-06-10 18:11:12 +0200

    Import gen6 compendiums for RU, PU, LC

---
+++ compendiums/gen6PU.js
@@ -0,0 +1,213 @@
+window.brmt = window.brmt || {};
+brmt.compendiums = brmt.compendiums || {};
+
+brmt.compendiums.gen6PU =
+`Zweilous, SSI, Tangela, Machoke, Mr. Mime, Clefairy, Relicanth, Klang, Avalugg
+Zweilous, NSI, Floatzel, Dodrio, Stoutland, Simipour, Ursaring, Rotom-Frost, Golduck, Fraxure, Articuno, Basculin, Ninjask, Gabite
+
+Floatzel, SSI, Roselia, Tangela, Grumpig, Clefairy, Gourgeist, Lickilicky, Politoed, Relicanth, Quilladin, Lapras
+Floatzel, NSI, Zebstrika, Simipour, Machoke, Regice, Sawsbuck, Articuno, Gogoat, Electabuzz, Ninjask
+
+Ursaring, GSI, Tangela
+Ursaring, SSI, Stunfisk, Misdreavus, Gourgeist, Relicanth, Avalugg
+Ursaring, NSI, Floatzel, Zebstrika, Kadabra, Raichu, Machoke, Purugly, Ninjask, Monferno
+
+Beartic, GSI, Metang
+Beartic, SSI, Grumpig, Politoed, Relicanth, Avalugg, Torkoal
+Beartic, NSI, Floatzel, Kadabra, Machoke, Golduck, Monferno, Ninjask, Armaldo, Huntail, Solrock
+
+Simisage, GSI, Swalot
+Simisage, SSI, Roselia, Arbok, Articuno, Lickilicky, Gourgeist, Vullaby
+Simisage, NSI, Floatzel, Zebstrika, Dodrio, Jumpluff, Kadabra, Rapidash, Ursaring, Simisear, Ninjask, Purugly, Monferno
+
+Simipour, GSI, Politoed
+Simipour, SSI, Roselia, Grumpig, Lickilicky, Clefairy, Gogoat, Vullaby, Zweilous
+Simipour, NSI, Floatzel, Zebstrika, Jumpluff, Raichu, Rapidash, Kadabra, Articuno, Basculin, Ninjask, Purugly
+
+Rotom-Frost, GSI, Lickilicky
+Rotom-Frost, SSI, Grumpig, Ninetales, Regice, Probopass, Clefairy, Klang, Electabuzz
+Rotom-Frost, NSI, Zebstrika, Rapidash, Ursaring, Raichu, Machoke, Simisear, Monferno
+
+Simisear, GSI, Politoed
+Simisear, SSI, Rapidash, Monferno, Grumpig, Lickilicky, Pelipper, Gabite, Vibrava
+Simisear, NSI, Floatzel, Zebstrika, Simipour, Jumpluff, Kadabra, Ursaring, Fraxure, Purugly, Machoke
+
+Armaldo, GSI, Relicanth
+Armaldo, SSI, Tangela, Stunfisk, Machoke, Misdreavus, Gourgeist, Gabite, Klang
+Armaldo, NSI, Floatzel, Simipour, Raichu, Simisage, Ninetales, Arbok, Golduck, Politoed, Simisear, Swanna, Basculin
+
+Lickilicky, GSI, Avalugg
+Lickilicky, SSI, Tangela, Machoke, Misdreavus, Golem, Gourgeist, Relicanth, Gabite, Zweilous, Solrock
+Lickilicky, NSI, Floatzel, Dodrio, Stoutland, Simipour, Raichu, Ursaring, Arbok, Basculin, Monferno
+
+Dusknoir, GSI, Zweilous
+Dusknoir, SSI, Stoutland, Tangela, Machoke, Ursaring, Golem, Pelipper, Relicanth, Vullaby, Avalugg
+Dusknoir, NSI, Floatzel, Dodrio, Simipour, Raichu, Simisage, Mightyena, Misdreavus, Basculin
+
+Stoutland, GSI, Tangela
+Stoutland, SSI, Stunfisk, Probopass, Relicanth, Golem, Metang, Klang, Solrock, Armaldo, Avalugg
+Stoutland, NSI, Floatzel, Simipour, Dodrio, Raichu, Grumpig, Ursaring, Rapidash, Simisage, Simisear, Monferno, Chatot
+
+Machoke, GSI, Arbok, Swalot
+Machoke, SSI, Articuno, Clefairy, Tangela, Gourgeist, Solrock
+Machoke, NSI, Jumpluff, Dodrio, Grumpig, Mr. Mime, Swanna, Ninjask, Murkrow
+
+Misdreavus, GSI, Machoke, Zweilous
+Misdreavus, SSI, Roselia, Stoutland, Ninetales, Rapidash, Regice, Vullaby
+Misdreavus, NSI, Dodrio, Simipour, Simisage, Simisear, Mightyena, Leafeon, Purugly, Kingler
+
+Pawniard, GSI, Stunfisk, Relicanth
+Pawniard, SSI, Machoke, Monferno, Tangela, Probopass, Gabite
+Pawniard, NSI, Floatzel, Zebstrika, Rapidash, Simipour, Rotom-Frost, Ninetales, Politoed
+
+Clefairy, GSI, Klang, Metang
+Clefairy, SSI, Roselia, Grumpig, Ninetales, Arbok, Misdreavus, Politoed
+Clefairy, NSI, Floatzel, Dodrio, Stoutland, Kadabra, Ursaring, Simisage, Golem, Basculin, Kingler, Marowak
+
+Kingler, GSI, Tangela, Quilladin
+Kingler, SSI, Relicanth, Gourgeist, Politoed, Vullaby, Gogoat, Leafeon
+Kingler, NSI, Floatzel, Simipour, Dodrio, Jumpluff, Kadabra, Simisage, Sawsbuck, Luxray
+
+Monferno, GSI, Stunfisk, Hippopotas
+Monferno, SSI, Grumpig, Duosion, Clefairy, Misdreavus, Gabite, Vullaby, Dusknoir
+Monferno, NSI, Floatzel, Simipour, Dodrio, Jumpluff, Machoke, Rapidash, Mr. Mime, Arbok, Golduck, Swanna, Solrock
+
+Fraxure, GSI, Tangela, Metang
+Fraxure, SSI, Stunfisk, Misdreavus, Clefairy, Gourgeist, Golem, Relicanth, Quilladin, Solrock, Avalugg
+Fraxure, NSI, Simipour, Kadabra, Machoke, Mr. Mime, Rotom-Frost, Ninjask
+
+Relicanth, GSI, Tangela, Gourgeist, Quilladin
+Relicanth, SSI, Machoke, Misdreavus, Leafeon, Metang, Gabite
+Relicanth, NSI, Floatzel, Zebstrika, Simipour, Kadabra, Raichu, Rotom-Frost, Simisage
+
+Raichu, GSI, Roselia, Stunfisk, Gogoat
+Raichu, SSI, Lickilicky, Grumpig, Regice, Gourgeist, Dusknoir
+Raichu, NSI, Floatzel, Zebstrika, Purugly, Kadabra, Machoke, Regice
+
+Leafeon, GSI, Tangela, Vullaby, Avalugg
+Leafeon, SSI, Jumpluff, Rapidash, Arbok, Monferno, Quilladin
+Leafeon, NSI, Floatzel, Dodrio, Zebstrika, Simipour, Ursaring, Ninetales, Simisear, Swanna, Ninjask
+
+Duosion, GSI, Grumpig, Misdreavus, Klang
+Duosion, SSI, Probopass, Lickilicky, Politoed, Zweilous, Vullaby
+Duosion, NSI, Stoutland, Kadabra, Rapidash, Ursaring, Mightyena, Basculin, Dusknoir, Armaldo
+
+Bouffalant, GSI, Misdreavus, Gourgeist, Solrock
+Bouffalant, SSI, Machoke, Relicanth, Vullaby, Dusknoir, Drifblim
+Bouffalant, NSI, Floatzel, Stoutland, Dodrio, Rapidash, Simipour, Ursaring, Monferno, Murkrow, Basculin
+
+Gorebyss, GSI, Roselia, Politoed, Lapras
+Gorebyss, SSI, Grumpig, Bouffalant, Clefairy, Lickilicky, Gogoat
+Gorebyss, NSI, Kadabra, Regice, Simipour, Sawsbuck, Rotom-Frost, Chatot
+
+Swanna, GSI, Regice, Lickilicky, Politoed
+Swanna, SSI, Grumpig, Vullaby, Klang, Zweilous, Electabuzz, Lapras
+Swanna, NSI, Floatzel, Dodrio, Zebstrika, Simipour, Kadabra, Rapidash, Ursaring, Simisage, Rotom-Frost
+
+Golduck, GSI, Politoed, Lickilicky, Lapras
+Golduck, SSI, Roselia, Grumpig, Regice, Clefairy, Zweilous, Gogoat
+Golduck, NSI, Floatzel, Simipour, Kadabra, Machoke, Articuno, Volbeat, Electabuzz, Mightyena
+
+Klang, GSI, Stunfisk, Gabite, Torkoal
+Klang, SSI, Rapidash, Tangela, Machoke, Misdreavus, Probopass, Politoed, Metang, Krokorok
+Klang, NSI, Floatzel, Simipour, Zebstrika, Raichu, Ninetales, Simisear, Monferno, Marowak
+
+Beheeyem, GSI, Lickilicky, Metang, Vullaby
+Beheeyem, SSI, Grumpig, Regice, Probopass, Misdreavus, Klang, Zweilous
+Beheeyem, NSI, Dodrio, Stoutland, Simipour, Jumpluff, Rapidash, Ursaring, Simisage, Mightyena, Chatot, Ninjask, Leafeon, Basculin, Dusknoir
+
+Gogoat, GSI, Jumpluff, Tangela, Gourgeist
+Gogoat, SSI, Roselia, Misdreavus, Rotom-Frost, Articuno, Vullaby, Zweilous, Avalugg
+Gogoat, NSI, Dodrio, Simipour, Kadabra, Rapidash, Regice, Ninetales, Arbok, Swanna, Monferno, Ninjask
+
+Huntail, GSI, Roselia, Politoed, Gourgeist
+Huntail, SSI, Misdreavus, Pelipper, Metang, Gogoat, Vullaby, Zweilous, Avalugg
+Huntail, NSI, Simipour, Kadabra, Machoke, Sawsbuck, Rotom-Frost, Electabuzz
+
+Murkrow, GSI, Stunfisk, Klang, Carbink
+Murkrow, SSI, Probopass, Golem, Regice, Relicanth, Gabite, Metang, Avalugg, Solrock
+Murkrow, NSI, Rotom-Frost, Electabuzz
+
+Kadabra, GSI, Lickilicky, Vullaby, Klang
+Kadabra, SSI, Stunfisk, Grumpig, Regice, Misdreavus, Clefairy, Politoed, Metang, Zweilous, Hypno
+Kadabra, NSI, Floatzel, Stoutland, Zebstrika, Simipour, Ursaring, Raichu, Mightyena, Purugly, Dusknoir
+
+Regice, GSI, Grumpig, Metang, Clefairy, Hypno
+Regice, SSI, Machoke, Lickilicky, Monferno, Klang
+Regice, NSI, Stoutland, Rapidash, Ursaring, Kadabra, Ninetales, Simisear, Armaldo
+
+Rapidash, GSI, Gabite, Relicanth, Solrock, Golem
+Rapidash, SSI, Stunfisk, Probopass, Politoed, Armaldo
+Rapidash, NSI, Floatzel, Raichu, Zebstrika, Kadabra, Machoke, Basculin, Grumpig
+
+Grumpig, GSI, Lickilicky, Metang, Vullaby, Zweilous
+Grumpig, SSI, Stunfisk, Misdreavus, Clefairy, Gourgeist, Munchlax
+Grumpig, NSI, Dodrio, Stoutland, Jumpluff, Ursaring, Simisage, Sawsbuck, Mightyena, Basculin, Dusknoir
+
+Golem, GSI, Tangela, Gourgeist, Quilladin, Solrock
+Golem, SSI, Roselia, Misdreavus, Leafeon, Gogoat, Vibrava
+Golem, NSI, Floatzel, Simipour, Jumpluff, Stoutland, Machoke, Simisage, Sawsbuck, Golduck, Politoed, Basculin
+
+Tangela, GSI, Roselia, Regice, Gourgeist, Vullaby
+Tangela, SSI, Zebstrika, Grumpig, Ninetales, Arbok, Articuno, Klang, Zweilous
+Tangela, NSI, Floatzel, Dodrio, Simipour, Rapidash, Rotom-Frost, Chatot, Simisear, Swanna
+
+Articuno, GSI, Probopass, Rotom-Frost, Klang, Metang
+Articuno, SSI, Regice, Grumpig, Ninetales, Lickilicky, Electabuzz, Munchlax
+Articuno, NSI, Dodrio, Stoutland, Zebstrika, Rapidash, Raichu, Ursaring, Simisage, Ninetales, Simisear, Luxray
+
+Mr. Mime, GSI, Metang, Misdreavus, Dusknoir, Dusclops
+Mr. Mime, SSI, Lickilicky, Stunfisk, Grumpig, Regice, Clefairy, Hypno
+Mr. Mime, NSI, Stoutland, Dodrio, Politoed, Klang, Mightyena
+
+Ninetales, GSI, Grumpig, Rapidash, Vibrava, Zweilous
+Ninetales, SSI, Machoke, Fraxure, Probopass, Lickilicky, Pelipper, Gabite, Stunfisk
+Ninetales, NSI, Floatzel, Raichu, Ursaring, Simipour, Simisear, Basculin, Purugly
+
+Basculin, GSI, Relicanth, Avalugg, Politoed, Lapras
+Basculin, SSI, Roselia, Tangela, Gourgeist, Leafeon, Gogoat, Quilladin, Zweilous
+Basculin, NSI, Floatzel, Dodrio, Zebstrika, Simipour, Jumpluff, Kadabra, Raichu, Ursaring, Sawsbuck, Simisage, Rotom-Frost, Electabuzz, Servine
+
+Chatot, GSI, Mr. Mime, Misdreavus, Dusknoir, Dusclops
+Chatot, SSI, Stunfisk, Regice, Probopass, Gourgeist, Lickilicky, Klang, Metang, Relicanth
+Chatot, NSI, Floatzel, Dodrio, Zebstrika, Simipour, Kadabra, Rapidash, Raichu, Simisear, Basculin, Electabuzz
+
+Ninjask, GSI, Stunfisk, Probopass, Relicanth, Klang, Carbink
+Ninjask, SSI, Golem, Gabite, Avalugg
+Ninjask, NSI, Simipour, Purugly, Raichu, Sawsbuck, Metang
+
+Dodrio, GSI, Stunfisk, Probopass, Golem, Relicanth, Avalugg
+Dodrio, SSI, Gabite, Klang, Metang, Solrock, Electabuzz, Luxray
+Dodrio, NSI, Floatzel, Zebstrika, Simipour, Kadabra, Rapidash, Raichu, Rotom-Frost, Simisear, Purugly
+
+Zebstrika, GSI, Stunfisk, Roselia, Grumpig, Lickilicky, Trapinch
+Zebstrika, SSI, Vigoroth, Regice, Probopass, Golem, Gogoat, Electabuzz, Marowak
+Zebstrika, NSI, Stoutland, Raichu, Machoke, Fraxure, Ninetales, Ninjask, Purugly, Dusknoir
+
+Jumpluff, GSI, Zebstrika, Metang, Klang, Electabuzz, Avalugg
+Jumpluff, SSI, Stunfisk, Tangela, Misdreavus, Probopass, Arbok, Articuno, Gourgeist, Gabite, Vullaby, Zweilous
+Jumpluff, NSI, Floatzel, Stoutland, Rapidash, Raichu, Fraxure, Rotom-Frost, Ninjask, Purugly
+
+Arbok, GSI, Stunfisk, Misdreavus, Gabite, Avalugg, Dusclops
+Arbok, SSI, Probopass, Arbok, Golem, Gourgeist, Klang, Metang, Relicanth, Armaldo, Dusknoir, Hippopotas, Solrock, Vibrava
+Arbok, NSI, Floatzel, Simipour, Kadabra, Raichu, Ursaring, Chatot, Marowak
+
+Sawsbuck, GSI, Tangela, Gourgeist, Vullaby, Klang, Metang, Avalugg
+Sawsbuck, SSI, Roselia, Arbok, Misdreavus, Articuno
+Sawsbuck, NSI, Dodrio, Zebstrika, Jumpluff, Kadabra, Machoke, Rapidash, Ninetales, Simisear, Ninjask, Monferno
+
+Roselia, GSI, Grumpig, Arbok, Lickilicky, Metang, Klang, Vullaby
+Roselia, SSI, Rapidash, Ninetales, Machoke, Misdreavus, Probopass, Dusknoir, Quilladin
+Roselia, NSI, Floatzel, Dodrio, Stoutland, Ursaring, Jumpluff, Mr. Mime, Articuno, Monferno, Simisear
+
+Mightyena, GSI, Stunfisk, Tangela, Relicanth, Klang, Hippopotas, Quilladin
+Mightyena, SSI, Probopass, Golem, Gabite, Monferno, Avalugg, Furfrou
+Mightyena, NSI, Jumpluff, Ninetales, Rapidash, Machoke
+
+Purugly, GSI, Probopass, Golem, Relicanth, Pelipper, Klang, Avalugg
+Purugly, SSI, Stunfisk, Tangela, Metang, Misdreavus, Gourgeist, Dusknoir, Gabite
+Purugly, NSI, Floatzel, Zebstrika, Kadabra, Machoke, Monferno, Fraxure
+
+Electabuzz, GSI, Roselia, Grumpig, Stunfisk, Clefairy, Lickilicky, Gourgeist, Gogoat, Hippopotas
+Electabuzz, SSI, Tangela, Machoke, Regice, Misdreavus
+Electabuzz, NSI, Floatzel, Stoutland, Rapidash, Ursaring, Fraxure, Leafeon`;
