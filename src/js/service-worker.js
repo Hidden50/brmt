330f8786ba422b4335a0d6e33cff83807688cfb0
Hidden50 <hiddensnorespond@gmail.com>
2018-05-24 23:26:37 +0200

    Update to bootstrap 4.1.1

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -9,9 +9,9 @@ const reqFiles = [
 	`./sprites.png`,
 	`./stylesheet.min.css`,
 	`./scripts.min.js`,
-	`https://code.jquery.com/jquery-3.2.1.slim.min.js`,
-	`https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js`,
-	`https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js`
+	`https://code.jquery.com/jquery-3.3.1.slim.min.js`,
+	`https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js`,
+	`https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js`,
 ];
 
 let requestCount = 0;
